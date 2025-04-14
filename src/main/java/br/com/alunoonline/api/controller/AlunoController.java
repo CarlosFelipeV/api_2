a78bf6c502f9cd23829f784401c1212a68bc349a
CarlosFelipeV <felipevas.tech@gmail.com>
2025-04-14 18:20:53 -0300

    new commit

--- src/main/java/br/com/alunoonline/api/controller/AlunoController.java
+++ src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -4,7 +4,11 @@ import br.com.alunoonline.api.model.Aluno;
 import br.com.alunoonline.api.service.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/alunos")
@@ -17,5 +21,23 @@ public class AlunoController {
     @ResponseStatus(HttpStatus.CREATED)
     public void criarAluno(@RequestBody Aluno aluno) {
         alunoService.criarAluno(aluno);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Aluno> listarTodosAlunos(){
+        return alunoService.listarTodosAlunos();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Aluno> buscarAlunoPorId(@PathVariable Long id){
+        return alunoService.buscarAlunoPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Aluno> deletarAlunoPorId(@PathVariable Long id){
+        return alunoService.deletarAlunoPorId(id);
     }
 }
--- src/main/java/br/com/alunoonline/api/service/AlunoService.java
+++ src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -5,6 +5,9 @@ import br.com.alunoonline.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class AlunoService {
 
@@ -15,4 +18,15 @@ public class AlunoService {
         alunoRepository.save(aluno);
     }
 
+    public List<Aluno> listarTodosAlunos(){
+     return alunoRepository.findAll();
+    }
+
+    public Optional<Aluno> buscarAlunoPorId(Long id){
+        return alunoRepository.findById(id);
+    }
+
+    public Optional<Aluno> deletarAlunoPorId(Long id) {
+        return alunoRepository.findById(id);
+    }
 }
